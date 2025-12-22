78151b7afd7a80fe7760d7684f07f6dd9606eb6f
meghasyam437 <meghasyam437@gmail.com>
2025-12-22 21:42:47 +0530

    Add files via upload

---
+++ 22-12-2025/y1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int a,b;
+    printf("enter the value of a,b");
+    scanf("%d %d",&a,&b);
+    int product;
+    product=a*b;
+    printf("%d",product);
+    return 0;//Your Code goes here!
+    
+    
+}
