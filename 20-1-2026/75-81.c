f54ffb5865ad5eb56c313f3688e5107c520a2fca
meghasyam437 <meghasyam437@gmail.com>
2026-01-20 22:52:47 +0530

    Add files via upload

---
+++ 20-1-2026/75-81.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+    char otp[1000];
+    scanf("%s", otp);            // reads OTP as a string
+
+    int n = (int)strlen(otp);
+
+    if (n != 6) {
+        printf("Invalid");
+        return 0;
+    }
+
+    for (int i = 0; i < 6; i++) {
+        if (!isdigit((unsigned char)otp[i])) {
+            printf("Invalid");
+            return 0;
+        }
+    }
+
+    printf("Valid");
+    return 0;
+}
